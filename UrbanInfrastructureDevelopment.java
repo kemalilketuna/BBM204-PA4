cd6cb74de0e64a34e74dc1eefcb1293a3a7040d2
Kemal Ilke Tuna <kemalilketuna@gmail.com>
2024-05-15 01:44:50 +0300

    10

--- UrbanInfrastructureDevelopment.java
+++ UrbanInfrastructureDevelopment.java
@@ -1,5 +1,15 @@
-import java.io.Serializable;
+import java.io.*;
 import java.util.*;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class UrbanInfrastructureDevelopment implements Serializable {
     static final long serialVersionUID = 88L;
@@ -20,8 +30,45 @@ public class UrbanInfrastructureDevelopment implements Serializable {
      * @return a list of Project objects
      */
     public List<Project> readXML(String filename) {
-        List<Project> projectList = new ArrayList<>();
-        // TODO: YOUR CODE HERE
-        return projectList;
+        try{
+            List<Project> projectList = new ArrayList<>();
+            File xmlFile = new File(filename);
+            DocumentBuilderFactory dFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(xmlFile);
+            doc.getDocumentElement().normalize();
+            // Node root = doc.getElementsByTagName("Projects").item(0);
+            // NodeList nList = ((Element) root).getElementsByTagName("Project");
+            NodeList projectNodeList = doc.getElementsByTagName("Project");
+
+            for(int i = 0; i < projectNodeList.getLength(); i++){
+                Element projectNode = (Element) projectNodeList.item(i);
+                String projectName = projectNode.getElementsByTagName("Name").item(0).getTextContent();
+                List<Task> taskList = new ArrayList<>();
+                NodeList taskNodeList = projectNode.getElementsByTagName("Task");
+                for(int j = 0; j < taskNodeList.getLength(); j++){
+                    Element taskNode = (Element) taskNodeList.item(j);
+                    String taskDescription = taskNode.getElementsByTagName("Description").item(0).getTextContent();
+                    int taskDuration = Integer.parseInt(taskNode.getElementsByTagName("Duration").item(0).getTextContent());
+                    List<Integer> dependencyList = new ArrayList<>();
+                    Node dependdeciesNode = taskNode.getElementsByTagName("Dependencies").item(0);
+                    NodeList dependencyNodeList = ((Element) dependdeciesNode).getElementsByTagName("DependsOnTaskID");
+                    for(int k = 0; k < dependencyNodeList.getLength(); k++){
+                        Element dependencyNode = (Element) dependencyNodeList.item(k);
+                        // print dependencyNode.getTextContent()
+                        int dependencyID = Integer.parseInt(dependencyNode.getTextContent());
+                        dependencyList.add(dependencyID);
+                    }
+                    Task task = new Task(j, taskDescription, taskDuration, dependencyList);
+                    taskList.add(task);
+                }
+                Project project = new Project(projectName, taskList);
+                projectList.add(project);
+            }
+            return projectList;
+        
+        }catch(Exception e){
+            throw new IllegalArgumentException("Error reading XML file");
+        }
     }
 }
